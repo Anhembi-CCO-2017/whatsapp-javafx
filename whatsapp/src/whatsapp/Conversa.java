2a422d42957fd50cc06e86be388853c21e5382d2
Unknown <gabrielpalmeiras78@hotmail.com>
2017-11-09 11:10:11 -0200

    Atualizando os arquivos

--- whatsapp/src/whatsapp/Conversa.java
+++ whatsapp/src/whatsapp/Conversa.java
@@ -1,14 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package whatsapp;
 
-/**
- *
- * @author foreach
- */
+import java.util.ArrayList;
+
 public class Conversa {
     
+    private static ArrayList<Mensagem>listaMensagens = new ArrayList<>();
+    private String contato;
+    private String onlineUltimaVez;
+
+    public Conversa(String contato, String onlineUltimaVez) {
+        this.contato = contato;
+        this.onlineUltimaVez = onlineUltimaVez;
+    }
+
+    public Conversa() {
+    }
+    
+
+    public static ArrayList<Mensagem> getListaMensagens() {
+        return listaMensagens;
+    }
+
+    public static void setListaMensagens(ArrayList<Mensagem> listaMensagens) {
+        Conversa.listaMensagens = listaMensagens;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getOnlineUltimaVez() {
+        return onlineUltimaVez;
+    }
+
+    public void setOnlineUltimaVez(String onlineUltimaVez) {
+        this.onlineUltimaVez = onlineUltimaVez;
+    }
+    
+    
+    public void addMensagem(ArrayList<Mensagem>listaMensagens, String autor, Mensagem mensagem){
+    
+        listaMensagens.add(mensagem);
+    }
+    
+    public String buscarMensagem(ArrayList<Mensagem>listaMensagens, String palavra){
+    
+        String saida = "";
+        for (Mensagem msg : listaMensagens) {
+            String [] tokens = msg.getTexto().split(" ");
+            if (tokens.equals(msg)) 
+                saida = saida +msg.getTexto()+"\n";
+            
+        }
+        
+        return saida;
+    }
+    public String retornarMensagens(){
+    
+        String saida = "";
+        
+        for (Mensagem msg : listaMensagens) {
+        
+            saida = saida + msg.getTexto()+"\n";
+        }
+        
+        return saida;
+    }
 }
--- whatsapp/src/whatsapp/Mensagem.java
+++ whatsapp/src/whatsapp/Mensagem.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package whatsapp;
 
-/**
- *
- * @author foreach
- */
+import java.util.Calendar;
+
 public class Mensagem {
-    
+
+private String emissor;
+private String texto;
+private String [] status = {"Está sendo enviado","Foi enviada","Foi recebida","Foi lida"};
+Calendar dataHora = Calendar.getInstance();
+
+    public Mensagem(String emissor, String texto, Calendar dataHora) {
+        this.emissor = emissor;
+        this.texto = texto;
+        this.dataHora = dataHora;
+    }
+
+    public void setStatus(String[] status) {
+        this.status = status;
+    }
+
+    public String getEmissor() {
+        return emissor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public String[] getStatus() {
+        return status;
+    }
+
+    public Calendar getDataHora() {
+        return dataHora;
+    }
+
+
 }
