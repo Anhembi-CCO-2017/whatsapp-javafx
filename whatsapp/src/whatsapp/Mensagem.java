7adaab9a285096d978e9c53971ce5895d5ffcfcc
Lucas Golino <lucasgolino@gmail.com>
2017-11-26 23:38:02 -0200

    Add javaDOC para Mensagem.java

--- whatsapp/src/whatsapp/Mensagem.java
+++ whatsapp/src/whatsapp/Mensagem.java
@@ -3,6 +3,10 @@ package whatsapp;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ *
+ * @author Lucas Golino, Thiago Almeida, Matheus Eli, Gabriel Henrique, Gabriel Forster
+ */
 public class Mensagem {
 
     private Usuario emissor;
@@ -25,6 +29,10 @@ public class Mensagem {
         this.setStatus(status);
     }
 
+    /**
+     *  Set Status da Mensagem pelo Index
+     * @param i   Index da mensagem
+     */
     public void setStatus(int i){
         String status[] = {"Está sendo enviada","foi enviada",
             "Foi recebida","foi lida"};
@@ -35,35 +43,66 @@ public class Mensagem {
         }
     }
 
+    /**
+     *  Get o emissor da mensagem
+     * @return Usuario   Objeto usuario
+     */
     public Usuario getEmissor() {
         return emissor;
     }
 
+    /**
+     *  Get Status com valor do Index
+     * @return Int   index de status
+     */
     public int getStatusIndex() {
         return statusIndex;
     }
 
-
+    /**
+     *  Get nome do Emissor
+     * @return String   nome do emissor
+     */
     public String getEmissorName() {
         return emissor.getNome();
     }
 
+    /**
+     *  Get Texto da Mensagem
+     * @return String
+     */
     public String getTexto() {
         return texto;
     }
 
+    /**
+     *  Get Mensagem de Status
+     * @return String   Mensagem de Status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     *  Get Data e Hora de envio da mensagem
+     * @return Calendar
+     */
     public Calendar getDataHora() {
         return dataHora;
     }
     
+    /**
+     *  Get Data pelo objeto Calendar
+     * @return Date
+     */
     public Date getData() {
         return dataHora.getTime();
     }
 
+    /**
+     *  Get Hora formatada de envio
+     * @return String
+     */
     public String getHora() {
         return dataHora.get(Calendar.HOUR) + ":" + dataHora.get(Calendar.MINUTE);
     }
